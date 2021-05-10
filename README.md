31bff3526c9f26da729bd763a8773ddd19d35e9a
Aldair André <aldairandre99@gmail.com>
2021-05-10 12:38:25 +0100

    inicio

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
+<<<<<<< HEAD
 # Enned
 ENNED WEB SITE
+=======
+# Enned
+>>>>>>> fd7cdd4... Inicio
---
+++ js/app.js
@@ -0,0 +1,7 @@
+const btn_menu = document.querySelector('.menui')
+
+const abrir = document.querySelector(".nav-lista")
+
+btn_menu.addEventListener("click",() =>{
+    abrir.classList.toggle("abrir")
+})
