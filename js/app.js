dbeea8e82a568f9cd4788ab27b1bab624bebd36a
aldairandre <aldairandre99@gmail.com>
2022-01-05 10:10:48 -0300

    um novo comeco

--- js/app.js
+++
@@ -1,7 +0,0 @@
-const btn_menu = document.querySelector('.menui')
-
-const abrir = document.querySelector(".nav-lista")
-
-btn_menu.addEventListener("click",() =>{
-    abrir.classList.toggle("abrir")
-})
